9f8fbeffd41da8d040f1fc8eb1380732951c17c3
Nicolas Ferreira <164096992+GhostFalk@users.noreply.github.com>
2025-05-13 21:12:23 -0300

    classe_Pessoa

---
+++ Pessoa.java
@@ -0,0 +1,18 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public abstract class Pessoa {
+    protected String nome;
+    protected String email;
+    protected String cpf;
+    protected String telefone;
+}
+public class Cliente extends Pessoa {
+    private LocalDate dataCadastro;
+    private ArrayList<listaAnimais> listaAnimais;
+
+}
+
+public class Funcionario extends Pessoa {
+    protected 
+}
