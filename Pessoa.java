c1c612f522a2fe1b891d5d1b1f1e6a2b17420ef5
Nicolas Ferreira <164096992+GhostFalk@users.noreply.github.com>
2025-05-13 21:36:28 -0300

    Att2Pessoa

--- Pessoa.java
+++ Pessoa.java
@@ -7,21 +7,21 @@ public abstract class Pessoa {
     protected String cpf;
     protected String telefone;
 }
-public class Cliente extends Pessoa {
+class Cliente extends Pessoa {
     private LocalDate dataCadastro;
     private ArrayList<listaAnimais> listaAnimais;
 
 }
 
-public class Funcionario extends Pessoa {
+class Funcionario extends Pessoa {
     protected String cargo;
 
 }
-public class Veterinario extends Funcionario {
+class Veterinario extends Funcionario {
     private String nome;
     private String especialidade;
 }
-public class Recepcionista extends Funcionario {
+class Recepcionista extends Funcionario {
     private Boolean turno;
-    
+
 }
