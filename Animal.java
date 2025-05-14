f5f7bf33c584363d780b4bfb038faf94f7211d2a
Marina Boeira <163803014+Mariilbb@users.noreply.github.com>
2025-05-13 21:11:05 -0300

    class animal

---
+++ Animal.java
@@ -0,0 +1,8 @@
+public class Animal {
+    private String nome;
+    private String especie;
+    private int idade;
+    private String sexo;
+    private double peso;
+    private Cliente dono;
+}
