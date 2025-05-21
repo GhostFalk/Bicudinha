673e389daa333989d3e9420e909dcd9b698e0b58
Nicolas Ferreira <164096992+GhostFalk@users.noreply.github.com>
2025-05-20 21:34:20 -0300

    metodos e contrutores

--- Pessoa.java
+++ Pessoa.java
@@ -71,10 +71,52 @@ class Funcionario extends Pessoa {
 }
 
 class Veterinario extends Funcionario {
-    private String nome;
+    private String crmv;
     private String especialidade;
-}
-class Recepcionista extends Funcionario {
-    private Boolean turno;
 
+    public Veterinario(String nome, String email, String cpf, String telefone, String crmv, String especialidade) {
+        super(nome, email, cpf, telefone, "Veterinário");
+        this.crmv = crmv;
+        this.especialidade = especialidade;
+    }
+
+    @Override
+    public void exibirDados() {
+        System.out.println("Veterinário: " + nome + " | CRMV: " + crmv + " | Especialidade: " + especialidade);
+    }
+
+    public void realizarConsulta(Animal animal) {
+        System.out.println("Consulta realizada com " + animal.getNome());
+    }
+
+    public void prescreverTratamento(Animal animal, String descricao) {
+        System.out.println("Tratamento para " + animal.getNome() + ": " + descricao);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
+
+class Recepcionista extends Funcionario {
+    private boolean turno;
+
+    public Recepcionista(String nome, String email, String cpf, String telefone, boolean turno) {
+        super(nome, email, cpf, telefone, "Recepcionista");
+        this.turno = turno;
+    }
+
+    @Override
+    public void exibirDados() {
+        String turnoStr = turno ? "Manhã" : "Tarde";
+        System.out.println("Recepcionista: " + nome + " | Turno: " + turnoStr);
+    }
+
+    public void agendarConsulta(Consulta consulta) {
+        System.out.println("Consulta agendada para " + consulta.detalhesConsulta());
+    }
+
+    public void chamarCliente() {
+        System.out.println("Próximo cliente, por favor!");
+    }
 }
