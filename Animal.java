550d85b30fd5f0ef1c6099c0e41b85d1f0eeff22
Marina Boeira <163803014+Mariilbb@users.noreply.github.com>
2025-05-20 21:29:13 -0300

    metodos

--- Animal.java
+++ Animal.java
@@ -5,4 +5,29 @@ public class Animal {
     private String sexo;
     private double peso;
     private Cliente dono;
+
+    public Animal(String nome, String especie, int idade, String sexo, double peso, Cliente dono) {
+        this.nome = nome;
+        this.especie = especie;
+        this.idade = idade;
+        this.sexo = sexo;
+        this.peso = peso;
+        this.dono = dono;
+    }
+
+    public void exibirFicha() {
+        System.out.println("Animal: " + nome + ", Espécie: " + especie + ", Sexo: " + sexo + ", Peso: " + peso + "kg");
+    }
+
+    public void vacinar() {
+        System.out.println(nome + " foi vacinado.");
+    }
+
+    public void atualizarPeso(double novoPeso) {
+        this.peso = novoPeso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
 }
